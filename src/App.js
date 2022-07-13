532c6f6b4e8e842fd2a678d4af2c77aa3dd015c2
Abigiya Terefe <abigiya1122@gmail.com>
2022-07-13 07:58:29 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Super from './components/Super';
+import Footer from './components/Footer';
+
+
+
+function App() {
+  return (
+    <div className="App">
+ <Navbar />
+ <Super />
+ <Footer />
+    </div>
+  );
+}
+
+export default App;
