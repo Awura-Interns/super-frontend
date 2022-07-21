98bafdb40ce4799c27a22a9eb3208c29fe049f34
Abigiya Terefe <abigiya1122@gmail.com>
2022-07-21 09:46:12 -0500

    main page frontend secalton done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
-import App from './App';
-
+import Super from '../src/components/MainPage/Super'
+import Footer from '../src/components/MainPage/Footer'
+import ItemList from '../src/components/MainPage/ItemList'
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
 
-    <App />
-  
+    <Super />
+    <ItemList/>
+    < Footer />
+
+    
+
+  </React.StrictMode>
 );
 
 
+reportWebVitals();
